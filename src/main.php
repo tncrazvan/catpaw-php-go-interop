b6a63bfa44e86a8a33303c735b1a6e664683043a
Razvan <tangent.jotey@gmail.com>
2024-03-01 23:11:30 +0100

    chore(app): updating example to catpaw 2.3.1

--- src/main.php
+++ src/main.php
@@ -20,9 +20,8 @@ function main():Unsafe {
         $goffi = goffi(Contract::class, asFileName(__DIR__, './main.so'))
             ->try($error) or yield $error;
 
-        // This example compresses the src/main.php file into a .zip file.
-        // $goffi->compress(asFileName(__DIR__, './main.php'))
-        //     ->try($error) or yield $error;
+        
+        // $goffi->compress(asFileName(__DIR__, './main.php'));
 
         $goffi->hello('world');
     });
